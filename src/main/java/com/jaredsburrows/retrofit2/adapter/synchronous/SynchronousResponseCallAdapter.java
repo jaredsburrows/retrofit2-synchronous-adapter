1711bac6c45ad4a0917fb58f82774b0dc9c50e2e
Jared Burrows <jaredsburrows@gmail.com>
2018-09-20 09:05:15 -0700

    handle responses better

--- src/main/java/com/jaredsburrows/retrofit2/adapter/synchronous/SynchronousResponseCallAdapter.java
+++ src/main/java/com/jaredsburrows/retrofit2/adapter/synchronous/SynchronousResponseCallAdapter.java
@@ -3,12 +3,15 @@ package com.jaredsburrows.retrofit2.adapter.synchronous;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import javax.annotation.Nonnull;
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.CallAdapter;
 import retrofit2.Response;
 
 @SuppressWarnings("ConstantConditions")
 final class SynchronousResponseCallAdapter<R> implements CallAdapter<R, Response<R>> {
+  private static final MediaType DEFAULT_MEDIA_TYPE = MediaType.parse("text/plain");
   private final Type responseType;
 
   SynchronousResponseCallAdapter(Type responseType) {
@@ -32,10 +35,21 @@ final class SynchronousResponseCallAdapter<R> implements CallAdapter<R, Response
     // Stop here if something goes wrong
     if (response == null) return null;
 
-    // If successful(200 OK) and Response<T> type, return the response with body
-    if (response.isSuccessful()) return Response.success(response.body(), response.raw());
-
-    // If unsuccessful(non 200 OK) and Response<T> type, return the response with body
-    return Response.error(response.code(), response.errorBody());
+    if (response.isSuccessful()) {
+      // If successful(200 OK) and Response<T> type, return the response with body
+      return response.raw() == null
+        ? Response.success(response.body())
+        : Response.success(response.body(), response.raw());
+    } else {
+      // If unsuccessful(non 200 OK) and Response<T> type, return the response with body
+      final ResponseBody errorBody = response.errorBody();
+      if (errorBody == null) {
+        return Response.error(ResponseBody.create(DEFAULT_MEDIA_TYPE, ""), response.raw());
+      } else {
+        return response.raw() == null
+          ? Response.<R>error(response.code(), errorBody)
+          : Response.<R>error(errorBody, response.raw());
+      }
+    }
   }
 }
